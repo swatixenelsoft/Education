0655ab035e6be8342ceec083e4b2f33bacd07efa
swatixenelsoft <swati.sharma@xenelsoft.co.in>
2025-01-29 14:27:34 +0530

    first commit

---
+++ src/navigation/Bottomnavigator.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Bottomnavigator = () => {
+  return (
+    <View>
+      <Text>Bottomnavigator</Text>
+    </View>
+  )
+}
+
+export default Bottomnavigator
---
+++ src/navigation/Navigationcontainer.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Navigationcontainer = () => {
+  return (
+    <View>
+      <Text>Navigationcontainer</Text>
+    </View>
+  )
+}
+
+export default Navigationcontainer
